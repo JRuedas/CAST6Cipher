682b2d78a67ac1e3cbebddb983a2027424ba2aba
Jonathan <jrmvp1992@gmail.com>
2017-12-03 00:55:01 +0100

    tested methods

--- src/MainClass.java
+++ src/MainClass.java
@@ -23,6 +23,10 @@ public class MainClass {
 
     private static BlockCipher engine;
     private static BufferedBlockCipher cipher;
+    private final static int NUMBER_OF_EXPERIMENTS = 100;
+
+    private static int[] messsagesHistogram;
+    private static int[] keysHistogram;
 
     private static void experimentChangingMessage() {
         // 256 bits key long -> 32 byte long
@@ -36,7 +40,8 @@ public class MainClass {
 
         byte[] newCipherText = cipherMessage(key, similarMessage);
 
-        hammingDistance(cipherText, newCipherText);
+        int hammingDistance = hammingDistance(cipherText, newCipherText);
+        messsagesHistogram[hammingDistance]++;
     }
 
     private static void experimentChangingKey() {
@@ -51,7 +56,8 @@ public class MainClass {
 
         byte[] newCipherText = cipherMessage(similarKey, message);
 
-        hammingDistance(cipherText, newCipherText);
+        int hammingDistance = hammingDistance(cipherText, newCipherText);
+        keysHistogram[hammingDistance]++;
     }
 
     private static byte[] cipherMessage(byte[] key, byte[] message) {
@@ -95,26 +101,19 @@ public class MainClass {
         return randomGenerator.nextInt(maxExclusive);
     }
 
-//    private static byte[] generateSimilarSequence(byte[] sequence) {
-//        BitSet bitSet = BitSet.valueOf(sequence);
-//        int bitToChange = generateRandomBitNumber(sequence.length * 8);
-//        System.out.println(bitToChange + " complemented");
-//        bitSet.flip(bitToChange);
-//        return bitSet.toByteArray();
-//    }
     private static byte[] generateSimilarSequence(byte[] sequence) {
         byte[] similarSequence = new byte[sequence.length];
         System.arraycopy(sequence, 0, similarSequence, 0, sequence.length);
 
         int randomBit = generateRandomBitNumber(similarSequence.length * 8);
 
-        boolean bitValue = isBitSet(similarSequence, randomBit);
+        boolean bitValue = bitValue(similarSequence, randomBit);
 
         return complementBit(similarSequence, randomBit, bitValue);
     }
 
-    // True if bit = 0, false if = 1 
-    public static boolean isBitSet(byte[] sequence, int bit) {
+    // True if bit = 1, false if = 0 
+    public static boolean bitValue(byte[] sequence, int bit) {
         int selectedByte = bit / 8;
         // Position of this bit in a byte
         int bitPosition = bit % 8;
@@ -134,19 +133,50 @@ public class MainClass {
         return sequence;
     }
 
-    private static void hammingDistance(byte[] sequence1, byte[] sequence2) {
+    private static int hammingDistance(byte[] sequence1, byte[] sequence2) {
+        int hammingDistance = 0;
 
+        for (int i = 0; i < 8; i++) {
+            if (bitValue(sequence1, i) != bitValue(sequence2, i)) {
+                hammingDistance++;
+            }
+        }
+        return hammingDistance;
+    }
+    
+    private static int mean() {
+        return 0;
     }
 
     public static void main(String[] args) {
         engine = new CAST6Engine();
         cipher = new PaddedBufferedBlockCipher(engine);
+        messsagesHistogram = new int[128];
+        keysHistogram = new int[256];
+
+        for (int i = NUMBER_OF_EXPERIMENTS; i > 0; i--) {
+            experimentChangingMessage();
+        }
+
+        for (int i = NUMBER_OF_EXPERIMENTS; i > 0; i--) {
+            experimentChangingKey();
+        }
+
         //experimentChangingMessage();
         //experimentChangingKey();
         // TESTS
         //totalResultsRandomNumber();
         //testGenerateRandomSequence();
-        testGenerateSimilarSequence();
+        //testGenerateSimilarSequence();
+        //testHamming();
+    }
+
+    private static void printBits(byte[] sequence) {
+        String s1 = "";
+        for (byte b : sequence) {
+            s1 += String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0');
+        }
+        System.out.println(s1);
     }
 
     private static void totalResultsRandomNumber() {
@@ -196,14 +226,16 @@ public class MainClass {
     }
 
     private static void testHamming() {
-
-    }
-
-    private static void printBits(byte[] sequence) {
-        String s1 = "";
-        for (byte b : sequence) {
-            s1 += String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0');
-        }
-        System.out.println(s1);
+        byte[] sequence = generateRandomBitSequence(8);
+        printBits(sequence);
+        byte[] sequence2 = generateSimilarSequence(sequence);
+        printBits(sequence2);
+        byte[] sequence3 = generateSimilarSequence(sequence2);
+        printBits(sequence3);
+        System.out.println(hammingDistance(sequence, sequence2));
+        System.out.println(hammingDistance(sequence, sequence3));
+        byte[] newSequence = generateRandomBitSequence(8);
+        printBits(newSequence);
+        System.out.println(hammingDistance(sequence, newSequence));
     }
 }
